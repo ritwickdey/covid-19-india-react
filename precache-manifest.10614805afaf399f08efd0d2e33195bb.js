1348f3ee59bcb6f5d60bd6f2dcbc812d0ef76592
Ritwick Dey (from Travis CI) <ritwickdey@outlook.com>
2020-06-16 21:29:07 +0000

    Deploy ritwickdey/covid-19-india-react to github.com/ritwickdey/covid-19-india-react.git:gh-pages

---
+++ precache-manifest.10614805afaf399f08efd0d2e33195bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cb632ed3048ac0e618c59a2213b13e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4eee90952ae9022aef",
+    "url": "/static/css/main.2badc2ed.chunk.css"
+  },
+  {
+    "revision": "b4d29e6eeb1b441faf22",
+    "url": "/static/js/2.45720ab6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45720ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4eee90952ae9022aef",
+    "url": "/static/js/main.77f4f08b.chunk.js"
+  },
+  {
+    "revision": "6acd5a19f1158cfb36e3",
+    "url": "/static/js/runtime-main.c429f216.js"
+  }
+]);
